1fa359eb59d5bec9a3663013b51ba698fdbcc704
kaiosatiro <kaiosatiro@gmail.com>
2022-12-31 14:55:01 -0300

    Segundo curso django

--- receitas/views.py
+++ receitas/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Receita
+from pessoas.models import Pessoa
 
 
 def index(request):
-    receitas = Receita.objects.all()
+    receitas = Receita.objects.filter(publicada=True).order_by('-data_receita')
 
     dados = {
         'receitas' : receitas
@@ -12,8 +13,22 @@ def index(request):
 
 def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
-
+     
     dados = {
-        'receita' : receita
+        'receita' : receita,
     }
     return render(request, 'receitas/receita.html', dados)
+
+def buscar(request):  
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        # if buscar:
+        #     lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+
+    lista_receitas = Receita.objects.filter(publicada=True, nome_receita__icontains=nome_a_buscar).order_by('-data_receita')    
+    
+    dados = {
+        'receitas': lista_receitas,
+    }
+
+    return render(request, 'receitas/buscar.html', dados)
--- setup/settings.py
+++ setup/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'receitas',
+    'pessoas',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MIDIA_URL = '/media/'
